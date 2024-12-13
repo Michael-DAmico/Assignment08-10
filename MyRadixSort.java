6515f6d372eef50a947d09cd59f0d92310e543a7
Michael <michael.damico@maine.edu>
2024-12-12 22:17:39 -0500

    Update MyRadixSort.java

--- MyRadixSort.java
+++ MyRadixSort.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyRadixSort {
+    /**
+     * Sorts an array of Tweet objects based on their ID using the Radix Sort algorithm.
+     * The algorithm processes the digits of the ID starting from the least significant digit
+     * to the most significant digit.
+     *
+     * @param tweets An array of Tweet objects to be sorted.
+     */
     public void sort(Tweet[] tweets) {
         int maxID = 9999999; // Since ID is always a 7-digit number (2000000 to 2099999)
         List<Tweet>[] buckets = new ArrayList[10];
